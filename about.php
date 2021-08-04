93bd919a05fce6d2f7ba727763bff2678bf11f06
Ashar Farooq <ashar744@gmail.com>
2021-08-04 02:35:46 -0400

    updated about page

--- about.php
+++ about.php
@@ -4,7 +4,12 @@ include_once 'header.php'
 
 ?>
 
-<p class="text-warning container">This is a simple site designed to allow a user to login to be able to visualize their online media profile.</p>
+<br/>
+<br/>
+<br/>
+<br/>
+
+<h3 class="text-info container">This is a simple site designed to allow a user to login to be able to visualize their online media profile.</h3>
 
 
 <?php
