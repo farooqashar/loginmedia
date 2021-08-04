c332d4b22e1322983ba580863c83659d5624ca3d
Ashar Farooq <ashar744@gmail.com>
2021-08-04 02:30:00 -0400

    updated main page

--- index.php
+++ index.php
@@ -1,24 +1,25 @@
-<?php 
+<?php
 
 include_once 'header.php'
 
 ?>
 
-<h1>Home Page(TBD STYLED LATER)</h1>
+<h1>Login System</h1>
+<p class="text-warning container">This is a simple site designed to allow a user to login to be able to visualize their online media profile.</p>
 
-<?php 
-    if (isset($_SESSION["userUserName"])) {
-        echo "<p>Hello There" . $_SESSION["userId"] . "</p>";
-    }
+<?php
+if (isset($_SESSION["userUserName"])) {
+    echo "<p>Hello There" . $_SESSION["userId"] . "</p>";
+}
 
-    if (isset($_GET["message"])) {
-        if ($_GET["message"] == "SUCCESSFULLOGIN") {
-            echo "<p>Login Successful!</p>";
-        }
+if (isset($_GET["message"])) {
+    if ($_GET["message"] == "SUCCESSFULLOGIN") {
+        echo "<p>Login Successful!</p>";
     }
+}
 ?>
 
-<?php 
+<?php
 
 include_once 'footer.php'
 
