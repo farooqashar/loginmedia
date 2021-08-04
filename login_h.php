0a56137a50aeb48bd0c710fb670dc6304fbb6241
Ashar Farooq <ashar744@gmail.com>
2021-08-04 02:44:45 -0400

    added labels login

--- login_h.php
+++ login_h.php
@@ -7,16 +7,29 @@ include_once 'header.php'
 <h1>Sign In</h1>
 
 <form action="includes/login.include.php" method="POST">
+
+    <div>
+    <label className="form-label" htmlFor="nameusername">
+              Email/Username:
+    </label>
     <input rows="1"
               cols="47"
               required
               class="form-control"
               type="text" name="nameusername" placeholder="Enter Email or Username Here">
+
+        </div>
+
+<div>
+<label className="form-label" htmlFor="nameusername">
+              Password:
+    </label>
     <input rows="1"
               cols="47"
               required
               class="form-control"
               type="password" name="loginpassword" placeholder="Enter Password Here">
+</div>
 
     <button type="submit" name="submit">Sign In</button>
 
