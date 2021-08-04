aa3563ca56a4b8fd010da7bf7c0a1facf533b756
Ashar Farooq <ashar744@gmail.com>
2021-08-04 02:37:56 -0400

    added form groups

--- signup.php
+++ signup.php
@@ -7,13 +7,30 @@ include_once 'header.php'
 <h1>Sign Up</h1>
 
 <form action="includes/signup.include.php" method="post">
-    <input type="text" name="name" placeholder="Enter Full Name Here">
-    <input type="text" name="email" placeholder="Enter Email Here">
-    <input type="text" name="username" placeholder="Enter Username Here">
-    <input type="password" name="password" placeholder="Enter Password Here">
-    <input type="password" name="confirmpassword" placeholder="Enter Password Again">
 
+    <div class="form-group">
+    <input type="text" name="name" placeholder="Enter Full Name Here">
+</div>
+
+    <div class="form-group">
+    <input type="text" name="email" placeholder="Enter Email Here">
+    </div>
+
+    <div class="form-group">
+    <input type="text" name="username" placeholder="Enter Username Here">
+    </div>
+    
+    <div class="form-group">
+    <input type="password" name="password" placeholder="Enter Password Here">
+    </div>
+
+    <div class="form-group">
+    <input type="password" name="confirmpassword" placeholder="Enter Password Again">
+    </div>
+
+    <div class="form-group">
     <button type="submit" name="submit">Sign Up</button>
+    </div>
 
 </form>
 
