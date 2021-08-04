471a15530db14a33d90e9d281fee321c2887377b
Ashar Farooq <ashar744@gmail.com>
2021-08-04 02:43:23 -0400

    changed input login

--- login_h.php
+++ login_h.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 include_once 'header.php'
 
@@ -7,14 +7,22 @@ include_once 'header.php'
 <h1>Sign In</h1>
 
 <form action="includes/login.include.php" method="POST">
-    <input type="text" name="nameusername" placeholder="Enter Email or Username Here">
-    <input type="password" name="loginpassword" placeholder="Enter Password Here">
+    <input rows="1"
+              cols="47"
+              required
+              class="form-control"
+              type="text" name="nameusername" placeholder="Enter Email or Username Here">
+    <input rows="1"
+              cols="47"
+              required
+              class="form-control"
+              type="password" name="loginpassword" placeholder="Enter Password Here">
 
     <button type="submit" name="submit">Sign In</button>
 
 </form>
 
-<?php 
+<?php
 
 include_once 'footer.php'
 
