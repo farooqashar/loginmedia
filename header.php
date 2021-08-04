310544545c5fac11c14506654ec0b769046ac5b8
Ashar Farooq <ashar744@gmail.com>
2021-08-04 02:25:37 -0400

    added bootstrap

--- header.php
+++ header.php
@@ -1,29 +1,71 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <title>Login Pipeline</title>
-  </head>
-  <body>
 
-    <nav>
+<head>
+  <meta charset="UTF-8" />
+  <title>Login System</title>
+  <link rel="stylesheet" href="css/styles.css">
+  <link rel='stylesheet' href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"  crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 
-        <ul>
-            <li><a href="index.php">Home</a></li>
-              <li><a href="about.php">About</a></li>
+</head>
 
-            <?php 
-              if (isset($_SESSION["userUserName"])) {
-                echo "<li><a href='profile.php'>Profile</a></li>";
-                echo "<li><a href='includes/logout.include.php'>Log Out</a></li>";
-              } else {
-                echo "<li><a href='signup.php'>Sign Up</a></li>";
-                echo "<li> <a href='login_h.php'>Log In</a></li>";
-              }
-            ?>
-        </ul>
-    </nav>
-  </body>
+<body>
+
+  <!-- <nav>
+
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="about.php">About</a></li>
+
+      <?php
+      if (isset($_SESSION["userUserName"])) {
+        echo "<li><a href='profile.php'>Profile</a></li>";
+        echo "<li><a href='includes/logout.include.php'>Log Out</a></li>";
+      } else {
+        echo "<li><a href='signup.php'>Sign Up</a></li>";
+        echo "<li> <a href='login_h.php'>Log In</a></li>";
+      }
+      ?>
+    </ul>
+  </nav> -->
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Login System</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+      <?php
+      if (isset($_SESSION["userUserName"])) {
+        echo "<li class='nav-item'><a class='nav-link' href='profile.php'>Profile</a></li>";
+        echo "<li class='nav-item'><a class='nav-link' href='includes/logout.include.php'>Log Out</a></li>";
+      } else {
+        echo "<li class='nav-item'><a class='nav-link' href='signup.php'>Sign Up</a></li>";
+        echo "<li class='nav-item'> <a class='nav-link' href='login_h.php'>Log In</a></li>";
+      }
+      ?>
+
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+
+
+</body>
+
 </html>
-
-
